79b2b5ae7b5cea8649a2a16bf41abb793ff91322
GerardoQ <mgfq13@hotmail.com>
2021-09-28 18:05:55 -0600

    Subiendo las estadisticas de las regiones

---
+++ src/regiones/sur.js
@@ -0,0 +1,40 @@
+var contenido = document.querySelector('#contenido')
+var total = document.querySelector('#total')
+
+function traer() {
+
+    const url = "https://backend-estadisticas-quejas.herokuapp.com/region/3";
+
+    fetch(url)
+        .then(res => res.json())
+        .then(datos => {
+            console.log(datos)
+            tabla(datos)
+        })
+}
+
+function tabla(datos) {
+    // console.log(datos)
+
+    const longitu = datos.length
+    console.log(longitu);
+
+    contenido.innerHTML = '';
+    total.innerHTML = `Total de quejas en Region Sur: ${longitu}`;
+
+    for(let valor of datos){
+        // console.log(valor.nombre)
+        contenido.innerHTML += `
+        
+        <tr>
+            <th scope="row">${ valor.comercioNombre }</th>
+            <td>${ valor.deptoNombre }</td>
+            <td>${ valor.municipioNombre }</td>
+            <td>${ valor.quejaFecha}</td>
+            <td>${ valor.sucursalDireccion}</td>
+            <td>${ valor.quejaDescripcion}</td>
+        </tr>
+        
+        `
+    }
+}
